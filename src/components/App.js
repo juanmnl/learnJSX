dabad344ca23d0f352213a328ca62fca324cb8fe
juanmnl <juanmnlcornejo@gmail.com>
2016-12-07 19:02:32 -0500

    :boom: big bang :boom:

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './../styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>Hello learnJSX!</h1>
+    );
+  }
+}
+
+export default App;
