47d6e5560c3808e7eaf5bbf2bdc18c5b6683676e
juanmnl <juanmnlcornejo@gmail.com>
2016-12-07 19:19:07 -0500

    Add app elements and babel transform from cdn

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,44 @@ import React, { Component } from 'react';
 import './../styles/App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '/* add JSX here */',
+      output: '',
+      err: '',
+    }
+  }
+
+  update(e) {
+    let code = e.target.value;
+    try {
+      this.setState({
+        output: window.Babel
+          .transform(code, { presets: ['es2015', 'react']})
+          .code,
+          err: '',
+      })
+    }
+    catch(err) {
+      this.setState({err: err.message})
+    }
+  }
+
   render() {
     return (
-      <h1>Hello learnJSX!</h1>
+      <div>
+        <header>{this.state.err}</header>
+        <section className="container">
+          <textarea
+            onChange={this.update.bind(this)}
+            defaultValue={this.state.input}
+          />
+          <pre>
+            {this.state.output}
+          </pre>
+        </section>
+      </div>
     );
   }
 }
